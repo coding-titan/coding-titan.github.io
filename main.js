ba3c3a67beb82146402a1aeb57478cee2cbde965
Seb <sebastian.manieri@gmail.com>
2024-09-19 23:03:00 +0200

    added buttons that change what project is displayed

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-// This is where the JavaScript will go.
+
+// Variable declarations
+let infoBoxOne = document.getElementById("project-box-one")
+let infoBoxTwo = document.getElementById("project-box-two")
+let infoBoxThree = document.getElementById("project-box-three")
+let buttonOne = document.getElementById("button-one")
+let buttonTwo = document.getElementById("button-two")
+let buttonThree = document.getElementById("button-three")
+
+// Buttons
+buttonOne.onclick = function() {
+    infoBoxOne.style.display = "grid";
+    infoBoxTwo.style.display = "none";
+    infoBoxThree.style.display = "none";
+}
+
+buttonTwo.onclick = function() {
+    infoBoxOne.style.display = "none";
+    infoBoxTwo.style.display = "grid";
+    infoBoxThree.style.display = "none";
+}
+
+buttonThree.onclick = function() {
+    infoBoxOne.style.display = "none";
+    infoBoxTwo.style.display = "none";
+    infoBoxThree.style.display = "grid";
+}
