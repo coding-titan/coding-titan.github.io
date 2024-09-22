c5631429a3895c0410986dae62634d925cfc4a36
Seb <sebastian.manieri@gmail.com>
2024-09-22 18:19:11 +0200

    text is now always centered over image

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ buttonOne.onclick = function() {
     infoBoxTwo.style.zIndex = "1";
     infoBoxThree.style.opacity = "0";
     infoBoxThree.style.zIndex = "1";
+
     document.body.style.backgroundColor = "darkmagenta";
+
+    window.scroll({top: 350, left: 0, behavior: "smooth"});
 }
 
 buttonTwo.onclick = function() {
@@ -25,7 +28,10 @@ buttonTwo.onclick = function() {
     infoBoxTwo.style.zIndex = "10";
     infoBoxThree.style.opacity = "0";
     infoBoxThree.style.zIndex = "1";
+
     document.body.style.backgroundColor = "black";
+
+    window.scroll({top: 350, left: 0, behavior: "smooth"})
 }
 
 buttonThree.onclick = function() {
@@ -35,5 +41,8 @@ buttonThree.onclick = function() {
     infoBoxTwo.style.zIndex = "1";
     infoBoxThree.style.opacity = "1";
     infoBoxThree.style.zIndex = "10";
+
     document.body.style.backgroundColor = "sienna";
+
+    window.scroll({top: 350, left: 0, behavior: "smooth"})
 }
